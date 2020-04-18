aa1daf8e91fc65e32eadc02cf20cca0e12c3c94a
Excellog <63909581+Excellog@users.noreply.github.com>
2020-04-18 19:29:37 +0700

    baru
    tetap pakai boostrap bawaan aja itu yang perlu dicoding

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php 
+$host       = "localhost";
+$user       = "root";
+$password   = "";
+$database   = "db_komputer";
+$conn    = mysqli_connect($host, $user, $password, $database);
+?>
---
+++ login.php
@@ -0,0 +1,167 @@
+<?php
+  include_once("prep.php");
+
+  // function checkexist($username,$password){
+  //   $status = 0;
+  //   $var = exequtequery($mysqli,"Select * from pembeli");
+  //   foreach ($var as $key => $value) {
+  //     if ($value['username'] == $username) {
+  //         $status = 1;
+  //         if ($value['password'] == $password) {
+  //             $status = 2;
+  //         }
+  //     }
+  //   }
+  //   return $status;
+  // }
+
+  // if (isset($_POST['login'])) {
+  //   $cek_user = checkexist($_POST['username'],$_POST['password']);
+  //   if ($cek_user == 0) {
+  //     echo "<script>alert('User ini tidak terdaftar!!!')</script>";
+  //   }else if ($cek_user == 1) {
+  //     echo "<script>alert('Password Anda Salah!!!')</script>";
+  //   }else{
+  //     header("Location: index.html");
+  //   }
+  // }
+
+  if (isset($_POST['login'])) {
+    $cek = 0;
+    $var = exequtequery($mysqli,"Select * from pembeli");
+      foreach ($var as $key => $value) {
+          if ($value["username"] == $_POST["username"]) {
+              $cek = 1;
+              if ($value["password"][$key] != $_POST["password"]) {
+                  echo "<script>alert('password salah')</script>";
+              }
+          }
+      }
+    if ($cek == 0) {
+      echo "<script>alert('User ini tidak ada !!!!!!!!!!!!!!')</script>";
+    }
+  }
+?>
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> 
+<html class="no-js"> <!--<![endif]-->
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="description" content="Aviato E-Commerce Template">
+  
+  <meta name="author" content="Themefisher.com">
+
+  <title>Computer Solution</title>
+
+  <!-- Mobile Specific Meta-->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <!-- Favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+  
+  <!-- Themefisher Icon font -->
+  <link rel="stylesheet" href="plugins/themefisher-font/style.css">
+  <!-- bootstrap.min css -->
+  <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+  
+  <!-- Revolution Slider -->
+  <link rel="stylesheet" type="text/css" href="plugins/revolution-slider/revolution/fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
+  <link rel="stylesheet" type="text/css" href="plugins/revolution-slider/revolution/fonts/font-awesome/css/font-awesome.css">
+
+  <!-- REVOLUTION STYLE SHEETS -->
+  <link rel="stylesheet" type="text/css" href="plugins/revolution-slider/revolution/css/settings.css">
+  <link rel="stylesheet" type="text/css" href="plugins/revolution-slider/revolution/css/layers.css">
+  <link rel="stylesheet" type="text/css" href="plugins/revolution-slider/revolution/css/navigation.css">
+  
+  <!-- Main Stylesheet -->
+  <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body id="body">
+
+<section class="signin-page account">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <div class="block text-center">
+          <a class="logo" href="index.html">
+            <img src="images/logo.png" alt="" style="width: 500px;">
+            <hr style="display: block;
+            margin-top: 0.5em;
+            margin-bottom: 0.5em;
+            margin-left: auto;
+            margin-right: auto;
+            border-style: inset;
+            border-width: 1px;">
+          </a>
+          <h2 class="text-center">Welcome Back</h2>
+          <form class="text-left clearfix" method="post" action="" >
+            <div class="form-group">
+              <input type="email" class="form-control" name="username" placeholder="Email">
+            </div>
+            <div class="form-group">
+              <input type="password" class="form-control" name="password" placeholder="Password">
+            </div>
+            <div class="text-center">
+              <input type="submit" class="btn btn-main text-center" value="Login" name="login">
+            </div>
+          </form>
+          <p class="mt-20">New in this site ?<a href="signin.html"> Create New Account</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+    <!-- 
+    Essential Scripts
+    =====================================-->
+    
+    <!-- Main jQuery -->
+    <script src="plugins/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap 3.1 -->
+    <script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Bootstrap Touchpin -->
+    <script src="plugins/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js"></script>
+    <!-- Instagram Feed Js -->
+    <script src="plugins/instafeed-js/instafeed.min.js"></script>
+    <!-- Video Lightbox Plugin -->
+    <script src="plugins/ekko-lightbox/dist/ekko-lightbox.min.js"></script>
+    <!-- Count Down Js -->
+    <script src="plugins/SyoTimer/build/jquery.syotimer.min.js"></script>
+    
+    <!-- Revolution Slider -->
+    <script type="text/javascript" src="plugins/revolution-slider/revolution/js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="plugins/revolution-slider/revolution/js/jquery.themepunch.revolution.min.js"></script>
+    
+    <!-- Revolution Slider -->
+    <script type="text/javascript" src="plugins/revolution-slider/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+    <script type="text/javascript" src="plugins/revolution-slider/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+    <script type="text/javascript" src="plugins/revolution-slider/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+    <script type="text/javascript" src="plugins/revolution-slider/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+    <script type="text/javascript" src="plugins/revolution-slider/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+    <script type="text/javascript" src="plugins/revolution-slider/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script type="text/javascript" src="plugins/revolution-slider/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+    <script type="text/javascript" src="plugins/revolution-slider/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <script type="text/javascript" src="plugins/revolution-slider/revolution/js/extensions/revolution.extension.video.min.js"></script>
+    <script type="text/javascript" src="plugins/revolution-slider/revolution/js/extensions/revolution.extension.video.min.js"></script>
+    <script type="text/javascript" src="plugins/revolution-slider/assets/warning.js"></script>  
+
+
+
+    <!-- Google Mapl -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCC72vZw-6tGqFyRhhg5CkF2fqfILn2Tsw"></script>
+  <script type="text/javascript" src="plugins/google-map/gmap.js"></script>
+
+    <!-- Main Js File -->
+    <script src="js/script.js"></script>
+    
+
+
+  </body>
+  </html>
